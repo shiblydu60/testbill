df5bad298464ca04d4a0439ccd7f0ea8ca806cd4
shiblydu60 <shiblydu60@yahoo.com>
2021-01-29 15:30:49 +0600

    modified

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,54 +98,24 @@
                             }
                         ?>
                     </td>
-                    <td> {{ $bill->note }}</td>
-                   <td> {{ Carbon\Carbon::parse($bill->monitored_at)->format('D M d, Y h:i:s') }}</td>
-                    <td> {{ Carbon\Carbon::parse($bill->created_at)->format('D M d, Y h:i:s') }}</td>
-                </tr>
-            @endif
-        @endforeach
-    </tbody>       
-</table>
-@endif
-
-{{--
-@if(Auth::user()->hasRole('superadmin') || Auth::user()->hasRole('accounts'))
-<table style="width: 100%;" class="table table-hover table-striped table-bordered" id="example">
-    <thead>
-        <tr role="row">
-            <th>Bill Date</th>
-            <th>Amount</th>
-            <th>Source</th>
-            <th>Destination</th>
-            <th>Purpose</th>
-            <th>Comments</th>
-            <th>Status</th>
-            <th>Created</th>
-        </tr>
-    </thead>
-    <tbody>
-    
-        @foreach ($daysBills_admin as $bill)
-            @if($bill->project->isdeleted==0)
-                <tr role="row" >
-                    <td> {{ Carbon\Carbon::parse($bill->bill_date)->format('D M d, Y') }}</td>
-                    <td> {{ $bill->amount }}</td>
-                    <td> {{ $bill->source }}</td>
-                    <td> {{ $bill->destination }}</td>
-                    <td> {{ $bill->project->name }}</td>
-                    <td> {{ \Illuminate\Support\Str::limit($bill->comment, 200, $end='...') }} </td>
-                    <td>
+                    <td> 
                         <?php
-                            if($bill->status==1) {
+                            if($bill->superadmin_status==1) {
                                 echo 'Approved';
                             }
-                            else if($bill->status==2) {
+                            else if($bill->superadmin_status==2) {
                                 echo 'Rejected';
                             }
                             else {
-                                echo $bill->status;
+                                echo $bill->superadmin_status;
                             }
                         ?>
+                        
+                    </td>
+                    <td> 
+                            @if($bill->superadmin_monitored_at != null) 
+                                {{ Carbon\Carbon::parse($bill->superadmin_monitored_at)->format('D M d, Y h:i:s') }}
+                            @endif
                     </td>
                     <td> {{ Carbon\Carbon::parse($bill->created_at)->format('D M d, Y h:i:s') }}</td>
                 </tr>
@@ -153,7 +123,7 @@
         @endforeach
     </tbody>       
 </table>
-@endif  --}}
+@endif
 
 @if(Auth::user()->hasRole('superadmin'))
 <h3>Approve or Reject bill</h3>
@@ -291,7 +261,19 @@
                           }
                       ?>
                    </td>
-                   <td> {{ $bill->superadmin_status }}</td>
+                   <td>
+                       <?php
+                            if($bill->superadmin_status==1) {
+                                echo 'Approved';
+                            }
+                            else if($bill->superadmin_status==2) {
+                                echo 'Rejected';
+                            }
+                            else {
+                                echo $bill->superadmin_status;
+                            }
+                        ?>
+                    </td>
                    <td> 
                         @if($bill->superadmin_monitored_at != null) 
                             {{ Carbon\Carbon::parse($bill->superadmin_monitored_at)->format('D M d, Y h:i:s') }}
