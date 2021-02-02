92c32333b434a2de5c839f09081a88fee3380a4a
shiblydu60 <shiblydu60@yahoo.com>
2021-02-02 12:00:11 +0600

    modified

--- resources/views/Bills/listbilladmin.blade.php
+++ resources/views/Bills/listbilladmin.blade.php
@@ -53,12 +53,24 @@
                                 }
                             ?>
                         </td>
-                        <td> {{ $bill->note }}</td>
+                        <td>
+                            <?php
+                                 if($bill->superadmin_status==1) {
+                                     echo 'Approved';
+                                 }
+                                 else if($bill->superadmin_status==2) {
+                                     echo 'Rejected';
+                                 }
+                                 else {
+                                     echo $bill->superadmin_status;
+                                 }
+                             ?>
+                         </td>
                         <td> 
-                            @if($bill->monitored_at != null)
-                                {{ Carbon\Carbon::parse($bill->monitored_at)->format('D M d, Y h:i:s') }}
-                            @endif
-                        </td>
+                             @if($bill->superadmin_monitored_at != null) 
+                                 {{ Carbon\Carbon::parse($bill->superadmin_monitored_at)->format('D M d, Y h:i:s') }}
+                             @endif
+                         </td>
                         <td> {{ Carbon\Carbon::parse($bill->created_at)->format('D M d, Y h:i:s') }}</td>
                         <td><a href="/bills/{{ $bill->id }}/edit">Edit</a></td>
                     </tr>
